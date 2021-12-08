943afd17b7b1a1f0cffab4a16c324a64225d68f2
yaboiwierdal <yaboiwierdal@gmail.com>
2021-12-08 08:28:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-app.use(express.static('public'));
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.listen(port, () => {
     console.log(`Running on port ${port}.`);
