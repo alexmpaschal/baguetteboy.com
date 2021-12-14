663d1c1a719fb69b5e2d4b0c0754bafc441e493a
yaboiwierdal <yaboiwierdal@gmail.com>
2021-12-14 08:51:31 -0500

    Update site to use Bootstrap

---
+++ contact.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import dotenv from 'dotenv'
+import nodemailer from 'nodemailer'
+
+const router = express.Router()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+dotenv.config()
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_NAME,
+        pass: process.env.EMAIL_PASS
+    }
+})
+
+router.use(express.urlencoded({ extended: true }))
+router.use(express.static(__dirname + '/public'));
+
+router.get('/', (req, res) => {
+    res.render('./contact/contact')
+})
+
+router.post('/', (req, res) => {
+    transporter.sendMail({
+        from: `${req.body.name} ${req.body.email}`,
+        to: process.env.EMAIL_NAME,
+        subject: req.body.subject,
+        text: req.body.message
+    })
+
+    res.redirect('/')
+})
+
+export default router
--- main.js
+++ main.js
@@ -1,10 +1,27 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const path = require('path');
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import dotenv from 'dotenv'
+import contact from './contact.js'
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+const app = express()
 
-app.listen(port, () => {
-    console.log(`Running on port ${port}.`);
-});
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs')
+
+app.use('/contact', contact)
+
+app.get('/', (req, res) => {
+    res.render('./en/')
+})
+
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}.`)
+})
